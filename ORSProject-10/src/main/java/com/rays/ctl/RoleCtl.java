b93889c695ffe1caf22e16e63d7369e67bbb1b5e
krati2709 <kratitrivedi27@gmail.com>
2026-03-16 17:50:52 +0530

    minor changes

--- ORSProject-10/src/main/java/com/rays/ctl/RoleCtl.java
+++ ORSProject-10/src/main/java/com/rays/ctl/RoleCtl.java
@@ -1,19 +1,36 @@
 package com.rays.ctl;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.BaseCtl;
+import com.rays.common.DropdownList;
 import com.rays.common.ORSResponse;
 import com.rays.dto.RoleDTO;
 import com.rays.form.RoleForm;
 import com.rays.service.RoleServiceInt;
 
-
 @RestController
 @RequestMapping(value = "Role")
-public class RoleCtl extends BaseCtl<RoleDTO, RoleServiceInt, RoleForm>{
+public class RoleCtl extends BaseCtl<RoleDTO, RoleServiceInt, RoleForm> {
 	
+	@Autowired
+	RoleServiceInt roleService;
+
+	@GetMapping(value = "preload")
+	public ORSResponse preload() {
+		ORSResponse res = new ORSResponse(true);
+
+		RoleDTO dto = new RoleDTO();		
+		List<DropdownList> list = roleService.search(dto, userContext);
+		res.addResult("roleList", list);
+		
+		return res;
+	}
 
 }
